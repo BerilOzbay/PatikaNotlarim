dc75c12277cd47e777dc947dad4a9b20e483016f
Beril Ozbay <berilozbay97@gmail.com>
2022-05-21 23:22:10 +0300

    BolumSonuOdevi

--- JavaScript/bolumSonuOdevi/input.js
+++ JavaScript/bolumSonuOdevi/input.js
@@ -3,5 +3,12 @@ let name = prompt("Lutfen adinizi giriniz: ")
 let info = document.querySelector("#info")
 info.innerHTML += ` Merhaba! ${name} Hoş geldin!` 
 
+function Time(){
+let gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+let today = new Date();
 let date = document.querySelector("#date")
-date.innerHTML = ` ${new Date().getHours()};`
+date.innerHTML = ` ${today.getHours()} :${today.getMinutes()}: ${today.getSeconds()} ${gunler[today.getDay()]}`
+setTimeout(Time,1);
+}
+
+Time();
