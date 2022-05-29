a8b7afbb2901a6da0ab7b251e60a0fcb0e9fd899
Beril Ozbay <berilozbay97@gmail.com>
2022-05-29 21:46:05 +0300

    JavaScript Diziler

--- JavaScript/alistirmalar/diziler.js
+++ JavaScript/alistirmalar/diziler.js
@@ -41,3 +41,33 @@ console.log(
 console.log(
     Array.isArray( true )// -> false
 )
+
+
+//Array yapisinda ogeler ile calismak
+let items2 = [10,20,30,40, ]
+console.log("items - ilk hali :", items2)
+
+//Array sona oge eklemek -> push
+items2.push(50)
+console.log(items2)
+
+//Array basa oge eklemek -> unshift
+items2.unshift(5)
+console.log(items2)
+
+//Array en sondaki ogeyi cikarmak -> pop
+items2.pop()
+console.log(items2)
+
+
+//Array en bastaki ogeyi cikarmak -> shift
+let firstItem = items2.shift()
+console.log(firstItem,items2)
+
+//Array icindeki bir ogenin bilgisinin degistirilmesi
+items2[0] = 5;
+console.log(items2)
+
+//Arrayda son elemanın degistirilmesi
+items2[items2.length-1] =10000
+console.log(items2)
