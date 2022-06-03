f5401593961a72f7f9cd09f0c737d43ae2f5ea99
Beril Ozbay <berilozbay97@gmail.com>
2022-06-03 16:00:38 +0300

    Nesne

---
+++ JavaScript/alistirmalar/Nesne.js
@@ -0,0 +1,13 @@
+let arrayObj = [1,2,3]
+let object1 = {obj: 1}
+
+console.log("arrayObj", typeof(arrayObj))
+console.log("object1", typeof(object1))
+
+// Nesne olusturma yollari
+let item1 = new Object ()
+let item2 = {}
+
+
+console.log("item1", typeof(item1))
+console.log("item2",typeof(item2))
